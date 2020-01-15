926ba1cb638eea00a2c9e18caadd8b69e5649bbe
Thomas Jensen <44089656+thomasjensen1194@users.noreply.github.com>
2020-01-15 22:48:55 +0100

    Added demo button to frontpage

--- src/components/Frontpage.tsx
+++ src/components/Frontpage.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { LargeText, CenterText } from 'styles/text';
+import { Button } from 'semantic-ui-react';
+import { useHistory } from 'react-router-dom';
 
 export interface FrontpageProps {}
 
 const Frontpage: React.FC<FrontpageProps> = () => {
+  const history = useHistory();
+
   return (
     <CenterText>
-      <LargeText>This is the frontpage</LargeText>
+      <Button fluid basic color="blue" onClick={() => history.push('/demo')}>
+        Demo
+      </Button>
     </CenterText>
   );
 };
